9fddf1d43fc150cc5bcce76d061275df14abfe99
Istiak Hossain <istiakhossainofficial@gmail.com>
2024-09-16 12:43:24 +0600

    same commit just change in little for check in admin dashboard

--- resources/js/Pages/Dashboard.vue
+++ resources/js/Pages/Dashboard.vue
@@ -16,9 +16,12 @@ import { Head } from '@inertiajs/vue3';
     <div class="py-12">
       <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
         <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-          <div class="p-6 text-gray-900">Welcome to the Admin Dashboard!</div>
+          <h1>Hello</h1>
+          <div class="p-6 text-gray-900">This is test dashboard</div>
         </div>
       </div>
     </div>
   </AdminLayout>
+
+
 </template>
